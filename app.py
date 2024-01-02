210fca2795bc005d7e16d97842f331eb4923415a
Thiago Lange <thiagolange@Air-de-Thiago.bbrouter>
2024-01-02 15:59:52 -0300

    Add components and update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from dash import Dash,dcc, html
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
+from components import (
+    geographical_content,
+    content_classification
+)
 
-# Initialize the Dash app and import the Bootstrap theme to style the dashboard
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container(
@@ -21,6 +23,17 @@ app.layout = dbc.Container(
         html.Div(id='tab-content', className='p-4'),
     ]
 )
+
+
+# This callback function switches between tabs in a dashboard based on user selection.
+# It updates the 'tab-content' component with the layout of the newly selected tab.
+@app.callback(Output('tab-content', 'children'), [Input('tabs', 'active_tab')])
+def switch_tab(at):
+    if at == 'tab1':
+        return geographical_content.layout
+    elif at == 'tab2':
+        return content_classification.layout
+
 
 if __name__ == '__main__':
     app.run(debug=True)
